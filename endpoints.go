5f67da897605913d10b40096501491c5c676bcd8
Yerassyl <tleugazy98@gmail.com>
2021-07-06 18:13:28 +0600

    change login endpoint

--- endpoints.go
+++ endpoints.go
@@ -3,7 +3,6 @@ package start31
 import (
 	"encoding/json"
 	"github.com/google/uuid"
-	"github.com/gorilla/mux"
 	redis_lib "github.com/kirigaikabuto/common-lib31"
 	users "github.com/kirigaikabuto/users31"
 	"io/ioutil"
@@ -12,9 +11,6 @@ import (
 )
 
 type HttpEndpoints interface {
-	TestEndpoint() func(w http.ResponseWriter, r *http.Request)
-	TestEndpointWithParam(idParam string) func(w http.ResponseWriter, r *http.Request)
-	TestPostEndpoint() func(w http.ResponseWriter, r *http.Request)
 	RegisterEndpoint() func(w http.ResponseWriter, r *http.Request)
 	LoginEndpoint() func(w http.ResponseWriter, r *http.Request)
 	ProfileEndpoint() func(w http.ResponseWriter, r *http.Request)
@@ -28,91 +24,6 @@ type httpEndpoints struct {
 
 func NewHttpEndpoints(uS users.UsersStore, rS *redis_lib.RedisStore) HttpEndpoints {
 	return &httpEndpoints{usersStore: uS, redisStore: rS}
-}
-
-func (h *httpEndpoints) TestEndpoint() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		user := users.User{
-			Id:        "1232213",
-			Username:  "username123",
-			Password:  "asdssadsa",
-			FirstName: "1233",
-			LastName:  "13213",
-			Avatar:    "asdsadsadsadad",
-		}
-		respondJSON(w, http.StatusOK, user)
-		return
-	}
-}
-
-func (h *httpEndpoints) TestEndpointWithParam(idParam string) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		idStr, ok := vars[idParam]
-		if !ok {
-			respondJSON(w, http.StatusBadRequest, HttpError{
-				Message:    "Dont have user with that id",
-				StatusCode: http.StatusBadRequest,
-			})
-		}
-		var response users.User
-		usersData := []users.User{
-			{
-				Id:        "1",
-				Username:  "1",
-				Password:  "1",
-				FirstName: "1",
-				LastName:  "1",
-				Avatar:    "1",
-			},
-			{
-				Id:        "2",
-				Username:  "2",
-				Password:  "2",
-				FirstName: "2",
-				LastName:  "2",
-				Avatar:    "2",
-			},
-		}
-		if idStr == "1" {
-			response = usersData[0]
-		} else if idStr == "2" {
-			response = usersData[1]
-		} else {
-			respondJSON(w, http.StatusBadRequest, HttpError{
-				Message:    "Dont have user with that id",
-				StatusCode: http.StatusBadRequest,
-			})
-			return
-		}
-		respondJSON(w, http.StatusOK, response)
-		return
-	}
-}
-
-func (h *httpEndpoints) TestPostEndpoint() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		jsonData, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			respondJSON(w, http.StatusBadRequest, HttpError{
-				Message:    err.Error(),
-				StatusCode: http.StatusBadRequest,
-			})
-			return
-		}
-		user := &users.User{}
-		err = json.Unmarshal(jsonData, &user)
-		if err != nil {
-			respondJSON(w, http.StatusInternalServerError, HttpError{
-				Message:    err.Error(),
-				StatusCode: http.StatusInternalServerError,
-			})
-			return
-		}
-		user.Id = "3333"
-		respondJSON(w, http.StatusCreated, user)
-		return
-	}
 }
 
 func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
@@ -209,7 +120,7 @@ func (h *httpEndpoints) LoginEndpoint() func(w http.ResponseWriter, r *http.Requ
 			return
 		}
 		key := uuid.New().String()
-		err = h.redisStore.SetValue(key, user, 5*time.Minute)
+		err = h.redisStore.Save(key, user.Id, 5*time.Minute)
 		if err != nil {
 			respondJSON(w, http.StatusInternalServerError, HttpError{
 				Message:    err.Error(),
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.13
 require (
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
-	github.com/kirigaikabuto/common-lib31 v0.0.0-20210703130813-da1c2d11b983
+	github.com/kirigaikabuto/common-lib31 v0.0.0-20210706120850-840dc13781e9
 	github.com/kirigaikabuto/users31 v0.0.0-20210703132012-2ed019bdffaf
 )
